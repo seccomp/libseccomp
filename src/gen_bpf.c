8c663b7b24d7c36be5c0e9b2caaa72e3446fd8ef
Paul Moore <pmoore@redhat.com>
2012-02-02 15:15:55 -0500

    gen: fix some problems with label handling, do some cleanup, and minor optimization
    We currently store labels on the stack, which is a big no-no as the BPF
    helper functions do not makes copies of the label strings which result
    in lots of garbage in the labels when you try to resolve them.  Fun.
    
    Thanks to Eric Paris for finding this problem and providing a partial
    fix.
    
    This patch creates a label store which is used as a stable place to
    maintain the labels while we are creating the BPF representation.  In
    addition, we do a good deal of cleanup to the BPF code (still needs a
    whole lot more), and another simple optimization as shown below:
    
    The current code generates something like the following:
    
    #
    # filter pseudo code start
    #
    # filter code for syscall #1
     if (syscall != 1) goto syscall_1_end;
     if (a0 != 0x1) goto syscall_1_c0_next;
     action ALLOW;
     syscall_1_c0_next:
     if (a0 != 0x2) goto syscall_1_c1_next;
     action ALLOW;
     syscall_1_c1_next:
     if (a1 == 0x0) goto syscall_1_c2_next;
     action ALLOW;
     syscall_1_c2_next:
     syscall_1_end:
    # default action
     action DENY;
    #
    # filter pseudo code end
    #
    
    This patch changes the generated code to look like the following:
    
    #
    # filter pseudo code start
    #
    # filter code for syscall #1
     if (syscall != 1) goto syscall_1_end;
     if (a0 != 0x1) goto syscall_1_c0_next;
     action ALLOW;
     syscall_1_c0_next:
     if (a0 != 0x2) goto syscall_1_c1_next;
     action ALLOW;
     syscall_1_c1_next:
     if (a1 == 0x0) goto syscall_1_end;
     action ALLOW;
     syscall_1_end:
    # default action
     action DENY;
    #
    # filter pseudo code end
    #
    
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- src/gen_bpf.c
+++ src/gen_bpf.c
@@ -49,52 +49,28 @@
 
 /* allocation increment size */
 #define BPF_INCR	(128 * sizeof(struct seccomp_filter_block))
+#define LBL_INCR	16
+#define LBL_LEN		256
 
 struct bpf_filter {
 	struct seccomp_fprog *prog;
-	size_t alloc_len;
+	size_t prog_alen;
+
+	char **lbl_store;
+	size_t lbl_len;
+	size_t lbl_alen;
+
 	struct bpf_labels lbls;
 };
+
+#define _max(x,y) \
+	((x) > (y) ? (x) : (y))
 
 #define _bpf_blk_len(x) \
 	(sizeof(x) / sizeof(x[0]))
 
 #define _bpf_next(x) \
 	(&((x)->prog->filter[(x)->prog->len]))
-
-/**
- * Grow the maximum BPF filter program size
- * @param bpf the bpf_filter struct
- * @param len the requested length
- * 
- * This functions checks to see if the BPF filter program space needs to be
- * expanded to meet the requested size and realloc's the space if needed.
- * Returns zero on success, negative values on failure.
- * 
- */
-static int _gen_bpf_grow(struct bpf_filter *bpf, size_t len)
-{
-	void *buf;
-	size_t buf_len = bpf->alloc_len + (BPF_INCR > len ? BPF_INCR : len);
-
-	if (bpf->prog->len + len <= bpf->alloc_len)
-		return 0;
-
-	/* XXX - check to make sure the size isn't getting too large */
-	/* XXX - is there a size limit? */
-
-	buf = realloc(bpf->prog->filter, buf_len);
-	if (buf == NULL) {
-		free(bpf->prog->filter);
-		bpf->prog->filter = NULL;
-		bpf->prog->len = 0;
-		bpf->alloc_len = 0;
-		return -ENOMEM;
-	}
-	bpf->prog->filter = buf;
-	bpf->alloc_len = buf_len;
-	return 0;
-}
 
 /**
  * Append BPF instructions to the main BPF program
@@ -111,12 +87,70 @@ static int _gen_bpf_append(struct bpf_filter *bpf,
 			   const struct seccomp_filter_block *blk,
 			   size_t blk_len)
 {
-	int rc = _gen_bpf_grow(bpf, blk_len);
-	if (rc < 0)
-		return rc;
+	void *buf;
+	size_t buf_len;
+
+	if (bpf->prog->len + blk_len > bpf->prog_alen) {
+		/* XXX - check to make sure the size isn't getting too large */
+		/* XXX - is there a size limit? */
+		buf_len = bpf->prog_alen + _max(BPF_INCR, blk_len);
+		buf = realloc(bpf->prog->filter, buf_len);
+		if (buf == NULL) {
+			free(bpf->prog->filter);
+			bpf->prog->filter = NULL;
+			bpf->prog->len = 0;
+			bpf->prog_alen = 0;
+			return -ENOMEM;
+		}
+		bpf->prog->filter = buf;
+		bpf->prog_alen = buf_len;
+	}
 	memcpy(_bpf_next(bpf), blk, blk_len * sizeof(*blk));
 	bpf->prog->len += blk_len;
+
 	return 0;
+}
+
+/**
+ * Append a new BPF jump label to the label store
+ * @param bpf the bpf_filter struct
+ * @param lbl the jump label
+ * 
+ * This function adds a new label to the BPF label store.  Returns a pointer
+ * to the label string zero on success, NULL on error.
+ *
+ */
+static char *_gen_bpf_lbl_append(struct bpf_filter *bpf, const char *lbl)
+{
+	int iter;
+	char *lbl_new;
+	char **array;
+	size_t array_len;
+
+	if (bpf->lbl_len >= bpf->lbl_alen) {
+		/* XXX - note the current limit on labels in bpf_helper.{c,h} */
+		array_len = bpf->lbl_alen + _max(LBL_INCR, bpf->lbl_len + 1);
+		array = realloc(bpf->lbl_store, array_len * sizeof(char *));
+		if (array == NULL)
+			goto label_append_failure;
+		bpf->lbl_store = array;
+		bpf->lbl_alen = array_len;
+	}
+	lbl_new = strdup(lbl);
+	if (lbl_new == NULL)
+		goto label_append_failure;
+	bpf->lbl_store[bpf->lbl_len++] = lbl_new;
+
+	return lbl_new;
+
+label_append_failure:
+	for (iter = 0; iter < bpf->lbl_len; iter++)
+		free(bpf->lbl_store[iter]);
+	free(bpf->lbl_store);
+	bpf->lbl_store = NULL;
+	bpf->lbl_len = 0;
+	bpf->lbl_alen = 0;
+	return NULL;
 }
 
 /**
@@ -125,6 +159,7 @@ static int _gen_bpf_append(struct bpf_filter *bpf,
  * @param sys_num the syscall number
  * @param chain_num the chain number
  * @param arg the syscall argument chain
+ * @param lbl_next_sys the jump label for the next syscall
  * @param bpf the bpf_filter struct
  *
  * This function generates BPF for the given syscall argument chain. Returns
@@ -135,16 +170,28 @@ static int _gen_bpf_syscall_chain(enum scmp_flt_action act,
 				  unsigned int sys_num,
 				  unsigned int chain_num,
 				  struct db_syscall_arg_chain_list *chain,
+				  const char *lbl_next_sys;
 				  struct bpf_filter *bpf)
 {
 	int rc;
-	char lbl_end[256]; /* XXX - ungh */
+	char *lbl_end;
+	char lbl_next_chain[LBL_LEN]; /* XXX - ungh */
 	struct db_syscall_arg_list *a_iter;
 
-	/* XXX - note the current limit on labels in bpf_helper.{c,h} */
-	rc = snprintf(lbl_end, 256, "syscall_%d_c%d_next", sys_num, chain_num);
-	if (rc >= 256)
-		return -E2BIG;
+	/* set the next jump point */
+	if (chain->next != NULL) {
+		/* XXX - note the current limit on labels in bpf_helper.{c,h} */
+		rc = snprintf(lbl_next_chain, LBL_LEN,
+			      "syscall_%d_c%d_next", sys_num, chain_num);
+		if (rc >= LBL_LEN)
+			return -E2BIG;
+		lbl_end = _gen_bpf_lbl_append(bpf, lbl_next_chain);
+		if (lbl_end == NULL)
+			return -ENOMEM;
+	} else
+		/* we are the last argument chain so just jump to the end of
+		 * the syscall filter block */
+		lbl_end = lbl_next_sys;
 
 	/* run through the argument chains */
 	db_list_foreach(a_iter, chain->args) {
@@ -155,7 +202,7 @@ static int _gen_bpf_syscall_chain(enum scmp_flt_action act,
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		}
 
 		/* do the comparison */
@@ -165,42 +212,42 @@ static int _gen_bpf_syscall_chain(enum scmp_flt_action act,
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else if (a_iter->op == SCMP_CMP_LT) {
 			struct seccomp_filter_block blk[] = {
 				JGE(a_iter->datum, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else if (a_iter->op == SCMP_CMP_LE) {
 			struct seccomp_filter_block blk[] = {
 				JGT(a_iter->datum, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else if (a_iter->op == SCMP_CMP_EQ) {
 			struct seccomp_filter_block blk[] = {
 				JNE(a_iter->datum, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else if (a_iter->op == SCMP_CMP_GE) {
 			struct seccomp_filter_block blk[] = {
 				JLT(a_iter->datum, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else if (a_iter->op == SCMP_CMP_GT) {
 			struct seccomp_filter_block blk[] = {
 				JLE(a_iter->datum, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		}
 	}
 	
@@ -212,7 +259,7 @@ static int _gen_bpf_syscall_chain(enum scmp_flt_action act,
 		};
 		rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 		if (rc < 0)
-			return -ENOMEM;
+			return rc;
 	} else {
 		struct seccomp_filter_block blk[] = {
 			DENY,
@@ -220,7 +267,7 @@ static int _gen_bpf_syscall_chain(enum scmp_flt_action act,
 		};
 		rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 		if (rc < 0)
-			return -ENOMEM;
+			return rc;
 	}
 
 	return 0;
@@ -241,14 +288,10 @@ static int _gen_bpf_syscall(enum scmp_flt_action act,
 			    struct bpf_filter *bpf)
 {
 	int rc;
-	char lbl_end[256]; /* XXX - ungh */
+	char *lbl_end;
+	char lbl_next_sys[LBL_LEN]; /* XXX - ungh^2 */
 	struct db_syscall_arg_chain_list *c_iter;
 	unsigned int c_count = 0;
-
-	/* XXX - note the current limit on labels in bpf_helper.{c,h} */
-	rc = snprintf(lbl_end, 256, "syscall_%d_end", sys->num); /* XXX - ungh^2 */
-	if (rc >= 256)
-		return -E2BIG;
 
 	if (sys->chains == NULL) {
 		if (act == SCMP_ACT_ALLOW) {
@@ -257,29 +300,40 @@ static int _gen_bpf_syscall(enum scmp_flt_action act,
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		} else {
 			struct seccomp_filter_block blk[] = {
 				SYSCALL(sys->num, DENY),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		}
 	} else {
+		/* XXX - note the current limit on labels in bpf_helper.{c,h} */
+		rc = snprintf(lbl_next_sys, LBL_LEN, "syscall_%d_end",
+			      sys->num);
+		if (rc >= LBL_LEN)
+			return -E2BIG;
+		lbl_end = _gen_bpf_lbl_append(bpf, lbl_next_sys);
+		if (lbl_end == NULL)
+			return -ENOMEM;
+
+		/* jump past this syscall block if it isn't the right one */
 		{
 			struct seccomp_filter_block blk[] = {
 				_SYSCALL(sys->num, _JUMP(&bpf->lbls, lbl_end)),
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		}
 
-		/* iterate over the arguments */
+		/* iterate over the argument chains */
 		db_list_foreach(c_iter, sys->chains) {
 			rc = _gen_bpf_syscall_chain(act, sys->num,
-						    c_count++, c_iter, bpf);
+						    c_count++, c_iter,
+						    lbl_end, bpf);
 			if (rc < 0)
 				return rc;
 		}
@@ -291,7 +345,7 @@ static int _gen_bpf_syscall(enum scmp_flt_action act,
 			};
 			rc = _gen_bpf_append(bpf, blk, _bpf_blk_len(blk));
 			if (rc < 0)
-				return -ENOMEM;
+				return rc;
 		}
 	}
 
@@ -308,9 +362,9 @@ static int _gen_bpf_syscall(enum scmp_flt_action act,
  */
 struct seccomp_fprog *gen_bpf_generate(const struct db_filter *db)
 {
-	int rc;
+	int rc, iter;
 	struct bpf_filter bpf;
-	struct db_syscall_list *iter;
+	struct db_syscall_list *sys_iter;
 
 	memset(&bpf, 0, sizeof(bpf));
 
@@ -328,15 +382,15 @@ struct seccomp_fprog *gen_bpf_generate(const struct db_filter *db)
 		};
 		rc = _gen_bpf_append(&bpf, blk, _bpf_blk_len(blk));
 		if (rc < 0)
-			goto bpf_generate_failure;
+			goto generate_failure;
 	}
 
-	db_list_foreach(iter, db->syscalls) {
+	db_list_foreach(sys_iter, db->syscalls) {
 		rc = _gen_bpf_syscall((db->def_action == SCMP_ACT_DENY ?
 				       SCMP_ACT_ALLOW : SCMP_ACT_DENY),
-				      iter, &bpf);
+				      sys_iter, &bpf);
 		if (rc < 0)
-			goto bpf_generate_failure;
+			goto generate_failure;
 	}
 
 	/* default action */
@@ -346,27 +400,33 @@ struct seccomp_fprog *gen_bpf_generate(const struct db_filter *db)
 		};
 		rc = _gen_bpf_append(&bpf, blk, _bpf_blk_len(blk));
 		if (rc < 0)
-			goto bpf_generate_failure;
+			goto generate_failure;
 	} else {
 		struct seccomp_filter_block blk[] = {
 			DENY,
 		};
 		rc = _gen_bpf_append(&bpf, blk, _bpf_blk_len(blk));
 		if (rc < 0)
-			goto bpf_generate_failure;
+			goto generate_failure;
 	}
 
 	/* resolve the labels/jumps */
-	rc = bpf_resolve_jumps(&bpf.lbls,
-			       bpf.prog->filter,
-			       bpf.prog->len);
+	rc = bpf_resolve_jumps(&bpf.lbls, bpf.prog->filter, bpf.prog->len);
 	if (rc != 0)
-		goto bpf_generate_failure;
+		goto generate_failure;
+
+	/* we don't need to keep the labels after we have resolved the jumps */
+	for (iter = 0; iter < bpf.lbl_len; iter++)
+		free(bpf.lbl_store[iter]);
+	free(bpf.lbl_store);
 
 	return bpf.prog;
 
-bpf_generate_failure:
-	free(bpf.prog);
+generate_failure:
+	gen_bpf_destroy(bpf.prog);
+	for (iter = 0; iter < bpf.lbl_len; iter++)
+		free(bpf.lbl_store[iter]);
+	free(bpf.lbl_store);
 	return NULL;
 }
 
@@ -382,6 +442,7 @@ void gen_bpf_destroy(struct seccomp_fprog *fprog)
 {
 	if (fprog == NULL)
 		return;
-	free(fprog->filter);
+	if (fprog->filter != NULL)
+		free(fprog->filter);
 	free(fprog);
 }
