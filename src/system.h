160e7c155e5ed73e40fdc85165665a6dc63a91a0
Paul Moore <pmoore@redhat.com>
2012-04-09 18:07:42 -0400

    all: relicense the library from GPLv2 to LGPLv2.1
    From the libseccomp-discuss mailing list:
    
      On Monday, April 09, 2012 06:06:51 PM Paul Moore wrote:
      > Hello,
      >
      > It was suggested on the libseccomp announcement thread that we
      > relicense the library from GPLv2 to LGPLv2.1.  In my opinion this
      > makes sense and I recommend we relicense the library, can I have
      > your permission to relicense your contributions?
      >
      >  * LGPLv2.1
      >    -> http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
    
      On Tuesday, April 10, 2012 10:07:37 AM Eric Paris wrote:
      > You have my permission to relicense to LGPL.
    
      On Tuesday, April 10, 2012 10:27:39 AM Ashley Lai wrote:
      > Yes, you have my permission to relicense to LGPL.
    
      On Tuesday, April 10, 2012 11:48:14 AM Corey Bryant wrote:
      > We (IBM) have OSSC approval now.  You have my approval to
      > relicense my contributions to LGPLv2.1.
    
      On Tuesday, April 10, 2012 12:57:25 PM Eduardo Otubo wrote:
      > On Tue, Apr 10, 2012 at 11:48:14AM -0400, Corey Bryant wrote:
      > > We (IBM) have OSSC approval now.  You have my approval to
      > > relicense my contributions to LGPLv2.1.
      >
      > Exactly, not a problem for me.
    
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- src/system.h
+++ src/system.h
@@ -6,17 +6,17 @@
  */
 
 /*
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of version 2 of the GNU General Public License as
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License as
  * published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses>.
  */
 
 #ifndef _SYSTEM_H
