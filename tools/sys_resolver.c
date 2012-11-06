f2e5bd048894ec53cdee9ec00911ca206f09b43b
Paul Moore <pmoore@redhat.com>
2012-11-06 17:30:07 -0500

    tools: make the sys_resolver tool work for non-native architectures
    In order to test non-native architectures using our existing regression
    test infrastructure and BPF simulator we need to be able to resolve
    syscall names on arbitrary architectures.
    
    This patch moves aside the existing bash script and replaces it with a
    small C progream which uses some internal libseccomp routines to resolve
    the syscall.
    
    Signed-off-by: Paul Moore <pmoore@redhat.com>

---
+++ tools/sys_resolver.c
@@ -0,0 +1,78 @@
+/**
+ * Syscall resolver
+ *
+ * Copyright (c) 2012 Red Hat <pmoore@redhat.com>
+ * Author: Paul Moore <pmoore@redhat.com>
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses>.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../src/arch.h"
+#include "../src/arch-i386.h"
+#include "../src/arch-x86_64.h"
+
+/**
+ * Print the usage information to stderr and exit
+ * @param program the name of the current program being invoked
+ *
+ * Print the usage information and exit with EINVAL.
+ *
+ */
+static void exit_usage(const char *program)
+{
+	fprintf(stderr,
+		"usage: %s [-h] [-a x86|x86_64] <syscall_name>\n", program);
+	exit(EINVAL);
+}
+
+/**
+ * main
+ */
+int main(int argc, char *argv[])
+{
+	int opt;
+	const struct arch_def *arch = &arch_def_native;
+
+	/* parse the command line */
+	while ((opt = getopt(argc, argv, "a:h"))> 0) {
+		switch (opt) {
+		case 'a':
+			if (strcmp(optarg, "x86") == 0)
+				arch = &arch_def_i386;
+			else if (strcmp(optarg, "x86_64") == 0)
+				arch = &arch_def_x86_64;
+			else
+				exit_usage(argv[0]);
+			break;
+		case 'h':
+		default:
+			/* usage information */
+			exit_usage(argv[0]);
+		}
+	}
+
+	/* sanity checks */
+	if (optind >= argc)
+		exit_usage(argv[0]);
+
+	printf("%d\n", arch_syscall_resolve_name(arch, argv[optind]));
+	return 0;
+}
