cecb030e75f3c996fda0e2753c998e50b92bb09a
Paul Moore <pmoore@redhat.com>
2013-02-19 17:26:01 -0500

    tests: add a test to exercise the x32 and arm architectures
    Signed-off-by: Paul Moore <pmoore@redhat.com>

---
+++ tests/23-sim-arch_all_basic.c
@@ -0,0 +1,93 @@
+/**
+ * Seccomp Library test program
+ *
+ * Copyright (c) 2012 Red Hat <pmoore@redhat.com>
+ * Author: Paul Moore <pmoore@redhat.com>
+ */
+
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of version 2.1 of the GNU Lesser General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses>.
+ */
+
+#include <unistd.h>
+
+#include <seccomp.h>
+
+#include "util.h"
+
+int main(int argc, char *argv[])
+{
+	int rc;
+	struct util_options opts;
+	scmp_filter_ctx ctx;
+
+	rc = util_getopt(argc, argv, &opts);
+	if (rc < 0)
+		goto out;
+
+	ctx = seccomp_init(SCMP_ACT_KILL);
+	if (ctx == NULL)
+		goto out;
+
+	if (seccomp_arch_exist(ctx, SCMP_ARCH_X86)) {
+		rc = seccomp_arch_add(ctx, SCMP_ARCH_X86);
+		if (rc != 0)
+			goto out;
+	}
+	if (seccomp_arch_exist(ctx, SCMP_ARCH_X86_64)) {
+		rc = seccomp_arch_add(ctx, SCMP_ARCH_X86_64);
+		if (rc != 0)
+			goto out;
+	}
+	if (seccomp_arch_exist(ctx, SCMP_ARCH_X32)) {
+		rc = seccomp_arch_add(ctx, SCMP_ARCH_X32);
+		if (rc != 0)
+			goto out;
+	}
+	if (seccomp_arch_exist(ctx, SCMP_ARCH_ARM)) {
+		rc = seccomp_arch_add(ctx, SCMP_ARCH_ARM);
+		if (rc != 0)
+			goto out;
+	}
+
+	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 1,
+			      SCMP_A0(SCMP_CMP_EQ, STDIN_FILENO));
+	if (rc != 0)
+		goto out;
+
+	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 1,
+			      SCMP_A0(SCMP_CMP_EQ, STDOUT_FILENO));
+	if (rc != 0)
+		goto out;
+
+	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 1,
+			      SCMP_A0(SCMP_CMP_EQ, STDERR_FILENO));
+	if (rc != 0)
+		goto out;
+
+	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(close), 0);
+	if (rc != 0)
+		goto out;
+
+	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0);
+	if (rc != 0)
+		goto out;
+
+	rc = util_filter_output(&opts, ctx);
+	if (rc)
+		goto out;
+
+out:
+	seccomp_release(ctx);
+	return (rc < 0 ? -rc : rc);
+}
