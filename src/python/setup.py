bb34e96b15a6c8928cabccc2418ef904c24368eb
Paul Moore <pmoore@redhat.com>
2014-04-23 10:24:06 -0400

    build: fixup the autotools build flags
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -40,6 +40,6 @@ setup(
 	cmdclass = {'build_ext': build_ext},
 	ext_modules = [
 		Extension("seccomp", ["seccomp.pyx"],
-			extra_objects=["../.libs/libseccomp.so"])
+			extra_objects=["../libseccomp-internal.la"])
 		]
 )
