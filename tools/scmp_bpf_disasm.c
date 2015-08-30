13386c1fd34834b6c9d46d36c43616b623736a93
Paul Moore <pmoore@redhat.com>
2015-08-29 21:13:21 -0400

    tools: add the missing s390/s390x support to scmp_bpf_disasm
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- tools/scmp_bpf_disasm.c
+++ tools/scmp_bpf_disasm.c
@@ -492,6 +492,10 @@ int main(int argc, char *argv[])
 				arch = AUDIT_ARCH_PPC64LE;
 			else if (strcmp(optarg, "ppc") == 0)
 				arch = AUDIT_ARCH_PPC;
+			else if (strcmp(optarg, "s390") == 0)
+				arch = AUDIT_ARCH_S390;
+			else if (strcmp(optarg, "s390x") == 0)
+				arch = AUDIT_ARCH_S390X;
 			else
 				exit_usage(argv[0]);
 			break;
