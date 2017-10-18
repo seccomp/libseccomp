01bce1bc177739d9fdb6bf0e05ed57bf58882997
Tyler Hicks <tyhicks@canonical.com>
2017-10-18 06:16:56 +0000

    tests: test suite infrastructure changes for SCMP_ACT_LOG
    The basics needed to handle tests that use the new SCMP_ACT_LOG
    action.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- tests/util.c
+++ tests/util.c
@@ -176,6 +176,8 @@ int util_action_parse(const char *action)
 		return -1; /* not yet supported */
 	else if (strcasecmp(action, "ALLOW") == 0)
 		return SCMP_ACT_ALLOW;
+	else if (strcasecmp(action, "LOG") == 0)
+		return SCMP_ACT_LOG;
 
 	return -1;
 }
