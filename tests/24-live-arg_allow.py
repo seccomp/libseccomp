8b38325d055261f2105ca96555d111bf595cca03
Paul Moore <paul@paul-moore.com>
2018-01-10 15:59:23 -0500

    tests: fix the live Python tests
    Add the sigaltstack(2) syscall to the list of allowed syscalls; tested
    against Python v3.6.4.
    
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (imported from commit 8aced6fe4087e7e16e76d06bdf1e73f0a2ae9c97)

--- tests/24-live-arg_allow.py
+++ tests/24-live-arg_allow.py
@@ -43,6 +43,7 @@ def test():
     f.add_rule(ALLOW, "close")
     f.add_rule(ALLOW, "rt_sigaction")
     f.add_rule(ALLOW, "rt_sigreturn")
+    f.add_rule(ALLOW, "sigaltstack")
     f.add_rule(ALLOW, "exit_group")
     f.add_rule(ALLOW, "brk")
     f.load()
