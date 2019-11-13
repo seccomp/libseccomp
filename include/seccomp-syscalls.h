seccomp/libseccomp

commit bf747eb21e428c2b3ead6ebcca27951b681963a0

arch: update the internal syscall tables to Linux v5.4-rc4
This is long overdue so quite a few changes, including tweaks to
support some newly direct wired syscalls which were previously
multiplexed.

We really need to make sure we update the syscall table more often.

Acked-by: Tom Hromatka <tom.hromatka@oracle.com>
Signed-off-by: Paul Moore <paul@paul-moore.com>

include/seccomp-syscalls.h
@@ -242,6 +242,36 @@
 #define __PNR_statx				-10208
 #define __PNR_io_pgetevents			-10209
 #define __PNR_rseq				-10210
+#define __PNR_setrlimit				-10211
+#define __PNR_clock_adjtime64			-10212
+#define __PNR_clock_getres_time64		-10213
+#define __PNR_clock_gettime64			-10214
+#define __PNR_clock_nanosleep_time64		-10215
+#define __PNR_clock_settime64			-10216
+#define __PNR_clone3				-10217
+#define __PNR_fsconfig				-10218
+#define __PNR_fsmount				-10219
+#define __PNR_fsopen				-10220
+#define __PNR_fspick				-10221
+#define __PNR_futex_time64			-10222
+#define __PNR_io_pgetevents_time64		-10223
+#define __PNR_move_mount			-10224
+#define __PNR_mq_timedreceive_time64		-10225
+#define __PNR_mq_timedsend_time64		-10226
+#define __PNR_open_tree				-10227
+#define __PNR_pidfd_open			-10228
+#define __PNR_pidfd_send_signal			-10229
+#define __PNR_ppoll_time64			-10230
+#define __PNR_pselect6_time64			-10231
+#define __PNR_recvmmsg_time64			-10232
+#define __PNR_rt_sigtimedwait_time64		-10233
+#define __PNR_sched_rr_get_interval_time64	-10234
+#define __PNR_semtimedop_time64			-10235
+#define __PNR_timer_gettime64			-10236
+#define __PNR_timer_settime64			-10237
+#define __PNR_timerfd_gettime64			-10238
+#define __PNR_timerfd_settime64			-10239
+#define __PNR_utimensat_time64			-10240
 
 /*
  * libseccomp syscall definitions
@@ -395,15 +425,51 @@
 
 #define __SNR_clock_adjtime		__NR_clock_adjtime
 
+#ifdef __NR_clock_adjtime64
+#define __SNR_clock_adjtime64		__NR_clock_adjtime64
+#else
+#define __SNR_clock_adjtime64		__PNR_clock_adjtime64
+#endif
+
 #define __SNR_clock_getres		__NR_clock_getres
+
+#ifdef __NR_clock_getres_time64
+#define __SNR_clock_getres_time64	__NR_clock_getres_time64
+#else
+#define __SNR_clock_getres_time64	__PNR_clock_getres_time64
+#endif
 
 #define __SNR_clock_gettime		__NR_clock_gettime
 
+#ifdef __NR_clock_gettime64
+#define __SNR_clock_gettime64		__NR_clock_gettime64
+#else
+#define __SNR_clock_gettime64		__PNR_clock_gettime64
+#endif
+
 #define __SNR_clock_nanosleep		__NR_clock_nanosleep
+
+#ifdef __NR_clock_nanosleep_time64
+#define __SNR_clock_nanosleep_time64	__NR_clock_nanosleep_time64
+#else
+#define __SNR_clock_nanosleep_time64	__PNR_clock_nanosleep_time64
+#endif
 
 #define __SNR_clock_settime		__NR_clock_settime
 
+#ifdef __NR_clock_settime64
+#define __SNR_clock_settime64		__NR_clock_settime64
+#else
+#define __SNR_clock_settime64		__PNR_clock_settime64
+#endif
+
 #define __SNR_clone			__NR_clone
+
+#ifdef __NR_clone3
+#define __SNR_clone3			__NR_clone3
+#else
+#define __SNR_clone3			__PNR_clone3
+#endif
 
 #define __SNR_close			__NR_close
 
@@ -563,7 +629,31 @@
 
 #define __SNR_fremovexattr		__NR_fremovexattr
 
+#ifdef __NR_fsconfig
+#define __SNR_fsconfig			__NR_fsconfig
+#else
+#define __SNR_fsconfig			__PNR_fsconfig
+#endif
+
 #define __SNR_fsetxattr			__NR_fsetxattr
+
+#ifdef __NR_fsmount
+#define __SNR_fsmount			__NR_fsmount
+#else
+#define __SNR_fsmount			__PNR_fsmount
+#endif
+
+#ifdef __NR_fsopen
+#define __SNR_fsopen			__NR_fsopen
+#else
+#define __SNR_fsopen			__PNR_fsopen
+#endif
+
+#ifdef __NR_fspick
+#define __SNR_fspick			__NR_fspick
+#else
+#define __SNR_fspick			__PNR_fspick
+#endif
 
 #ifdef __NR_fstat
 #define __SNR_fstat			__NR_fstat
@@ -616,6 +706,12 @@
 #endif
 
 #define __SNR_futex			__NR_futex
+
+#ifdef __NR_futex_time64
+#define __SNR_futex_time64		__NR_futex_time64
+#else
+#define __SNR_futex_time64		__PNR_futex_time64
+#endif
 
 #ifdef __NR_futimesat
 #define __SNR_futimesat			__NR_futimesat
@@ -849,6 +945,12 @@
 #define __SNR_io_pgetevents		__PNR_io_pgetevents
 #endif
 
+#ifdef __NR_io_pgetevents_time64
+#define __SNR_io_pgetevents_time64	__NR_io_pgetevents_time64
+#else
+#define __SNR_io_pgetevents_time64	__PNR_io_pgetevents_time64
+#endif
+
 #define __SNR_io_setup			__NR_io_setup
 
 #define __SNR_io_submit			__NR_io_submit
@@ -1025,6 +1127,12 @@
 
 #define __SNR_mount			__NR_mount
 
+#ifdef __NR_move_mount
+#define __SNR_move_mount		__NR_move_mount
+#else
+#define __SNR_move_mount		__PNR_move_mount
+#endif
+
 #ifdef __NR_move_pages
 #define __SNR_move_pages		__NR_move_pages
 #else
@@ -1047,7 +1155,19 @@
 
 #define __SNR_mq_timedreceive		__NR_mq_timedreceive
 
+#ifdef __NR_mq_timedreceive_time64
+#define __SNR_mq_timedreceive_time64	__NR_mq_timedreceive_time64
+#else
+#define __SNR_mq_timedreceive_time64	__PNR_mq_timedreceive_time64
+#endif
+
 #define __SNR_mq_timedsend		__NR_mq_timedsend
+
+#ifdef __NR_mq_timedsend_time64
+#define __SNR_mq_timedsend_time64	__NR_mq_timedsend_time64
+#else
+#define __SNR_mq_timedsend_time64	__PNR_mq_timedsend_time64
+#endif
 
 #define __SNR_mq_unlink			__NR_mq_unlink
 
@@ -1157,6 +1277,12 @@
 
 #define __SNR_open_by_handle_at		__NR_open_by_handle_at
 
+#ifdef __NR_open_tree
+#define __SNR_open_tree			__NR_open_tree
+#else
+#define __SNR_open_tree			__PNR_open_tree
+#endif
+
 #define __SNR_openat			__NR_openat
 
 #ifdef __NR_pause
@@ -1186,6 +1312,18 @@
 #define __SNR_perf_event_open		__NR_perf_event_open
 
 #define __SNR_personality		__NR_personality
+
+#ifdef __NR_pidfd_open
+#define __SNR_pidfd_open		__NR_pidfd_open
+#else
+#define __SNR_pidfd_open		__PNR_pidfd_open
+#endif
+
+#ifdef __NR_pidfd_send_signal
+#define __SNR_pidfd_send_signal		__NR_pidfd_send_signal
+#else
+#define __SNR_pidfd_send_signal		__PNR_pidfd_send_signal
+#endif
 
 #ifdef __NR_pipe
 #define __SNR_pipe			__NR_pipe
@@ -1221,7 +1359,11 @@
 #define __SNR_poll			__PNR_poll
 #endif
 
-#define __SNR_ppoll			__NR_ppoll
+#ifdef __NR_ppoll_time64
+#define __SNR_ppoll_time64		__NR_ppoll_time64
+#else
+#define __SNR_ppoll_time64		__PNR_ppoll_time64
+#endif
 
 #define __SNR_prctl			__NR_prctl
 
@@ -1250,6 +1392,12 @@
 #endif
 
 #define __SNR_pselect6			__NR_pselect6
+
+#ifdef __NR_pselect6_time64
+#define __SNR_pselect6_time64		__NR_pselect6_time64
+#else
+#define __SNR_pselect6_time64		__PNR_pselect6_time64
+#endif
 
 #define __SNR_ptrace			__NR_ptrace
 
@@ -1317,6 +1465,12 @@
 #define __SNR_recvmmsg			__PNR_recvmmsg
 #endif
 
+#ifdef __NR_recvmmsg_time64
+#define __SNR_recvmmsg_time64		__NR_recvmmsg_time64
+#else
+#define __SNR_recvmmsg_time64		__PNR_recvmmsg_time64
+#endif
+
 #ifdef __NR_recvmsg
 #define __SNR_recvmsg			__NR_recvmsg
 #else
@@ -1366,6 +1520,12 @@
 #define __SNR_rt_sigsuspend		__NR_rt_sigsuspend
 
 #define __SNR_rt_sigtimedwait		__NR_rt_sigtimedwait
+
+#ifdef __NR_rt_sigtimedwait_time64
+#define __SNR_rt_sigtimedwait_time64	__NR_rt_sigtimedwait_time64
+#else
+#define __SNR_rt_sigtimedwait_time64	__PNR_rt_sigtimedwait_time64
+#endif
 
 #define __SNR_rt_tgsigqueueinfo		__NR_rt_tgsigqueueinfo
 
@@ -1419,6 +1579,12 @@
 
 #define __SNR_sched_rr_get_interval	__NR_sched_rr_get_interval
 
+#ifdef __NR_sched_rr_get_interval_time64
+#define __SNR_sched_rr_get_interval_time64	__NR_sched_rr_get_interval_time64
+#else
+#define __SNR_sched_rr_get_interval_time64	__PNR_sched_rr_get_interval_time64
+#endif
+
 #define __SNR_sched_setaffinity		__NR_sched_setaffinity
 
 #define __SNR_sched_setattr		__NR_sched_setattr
@@ -1465,6 +1631,12 @@
 #define __SNR_semtimedop		__NR_semtimedop
 #else
 #define __SNR_semtimedop		__PNR_semtimedop
+#endif
+
+#ifdef __NR_semtimedop_time64
+#define __SNR_semtimedop_time64		__NR_semtimedop_time64
+#else
+#define __SNR_semtimedop_time64		__PNR_semtimedop_time64
 #endif
 
 #ifdef __NR_send
@@ -1637,7 +1809,11 @@
 #define __SNR_setreuid32		__PNR_setreuid32
 #endif
 
+#ifdef __NR_setrlimit
 #define __SNR_setrlimit			__NR_setrlimit
+#else
+#define __SNR_setrlimit			__PNR_setrlimit
+#endif
 
 #define __SNR_setsid			__NR_setsid
 
@@ -1921,7 +2097,19 @@
 
 #define __SNR_timer_gettime		__NR_timer_gettime
 
+#ifdef __NR_timer_gettime64
+#define __SNR_timer_gettime64		__NR_timer_gettime64
+#else
+#define __SNR_timer_gettime64		__PNR_timer_gettime64
+#endif
+
 #define __SNR_timer_settime		__NR_timer_settime
+
+#ifdef __NR_timer_settime64
+#define __SNR_timer_settime64		__NR_timer_settime64
+#else
+#define __SNR_timer_settime64		__PNR_timer_settime64
+#endif
 
 #ifdef __NR_timerfd
 #define __SNR_timerfd			__NR_timerfd
@@ -1933,7 +2121,19 @@
 
 #define __SNR_timerfd_gettime		__NR_timerfd_gettime
 
+#ifdef __NR_timerfd_gettime64
+#define __SNR_timerfd_gettime64		__NR_timerfd_gettime64
+#else
+#define __SNR_timerfd_gettime64		__PNR_timerfd_gettime64
+#endif
+
 #define __SNR_timerfd_settime		__NR_timerfd_settime
+
+#ifdef __NR_timerfd_settime64
+#define __SNR_timerfd_settime64		__NR_timerfd_settime64
+#else
+#define __SNR_timerfd_settime64		__PNR_timerfd_settime64
+#endif
 
 #define __SNR_times			__NR_times
 
@@ -2036,6 +2236,12 @@
 #endif
 
 #define __SNR_utimensat			__NR_utimensat
+
+#ifdef __NR_utimensat_time64
+#define __SNR_utimensat_time64		__NR_utimensat_time64
+#else
+#define __SNR_utimensat_time64		__PNR_utimensat_time64
+#endif
 
 #ifdef __NR_utimes
 #define __SNR_utimes			__NR_utimes

src/arch-aarch64-syscalls.c
@@ -26,7 +26,7 @@
 #include "arch.h"
 #include "arch-aarch64.h"
 
-/* NOTE: based on Linux 4.15-rc7 */
+/* NOTE: based on Linux v5.4-rc4 */
 const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "_llseek", __PNR__llseek },
 	{ "_newselect", __PNR__newselect },
@@ -58,11 +58,17 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "chown32", __PNR_chown32 },
 	{ "chroot", 51 },
 	{ "clock_adjtime", 266 },
+	{ "clock_adjtime64", __PNR_clock_adjtime64 },
 	{ "clock_getres", 114 },
+	{ "clock_getres_time64", __PNR_clock_getres_time64 },
 	{ "clock_gettime", 113 },
+	{ "clock_gettime64", __PNR_clock_gettime64 },
 	{ "clock_nanosleep", 115 },
+	{ "clock_nanosleep_time64", __PNR_clock_nanosleep_time64 },
 	{ "clock_settime", 112 },
+	{ "clock_settime64", __PNR_clock_settime64 },
 	{ "clone", 220 },
+	{ "clone3", __PNR_clone3 },
 	{ "close", 57 },
 	{ "connect", 203 },
 	{ "copy_file_range", 285 },
@@ -106,7 +112,11 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "flock", 32 },
 	{ "fork", __PNR_fork },
 	{ "fremovexattr", 16 },
+	{ "fsconfig", 431 },
 	{ "fsetxattr", 7 },
+	{ "fsmount", 432 },
+	{ "fsopen", 430 },
+	{ "fspick", 433 },
 	{ "fstat", 80 },
 	{ "fstat64", __PNR_fstat64 },
 	{ "fstatat64", __PNR_fstatat64 },
@@ -117,6 +127,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "ftruncate", 46 },
 	{ "ftruncate64", __PNR_ftruncate64 },
 	{ "futex", 98 },
+	{ "futex_time64", __PNR_futex_time64 },
 	{ "futimesat", __PNR_futimesat },
 	{ "get_kernel_syms", __PNR_get_kernel_syms },
 	{ "get_mempolicy", 236 },
@@ -148,7 +159,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "getresgid32", __PNR_getresgid32 },
 	{ "getresuid", 148 },
 	{ "getresuid32", __PNR_getresuid32 },
-	{ "getrlimit", 163 },
+	{ "getrlimit", __PNR_getrlimit },
 	{ "getrusage", 165 },
 	{ "getsid", 156 },
 	{ "getsockname", 204 },
@@ -169,6 +180,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "io_destroy", 1 },
 	{ "io_getevents", 4 },
 	{ "io_pgetevents", 292 },
+	{ "io_pgetevents_time64", __PNR_io_pgetevents_time64 },
 	{ "io_setup", 0 },
 	{ "io_submit", 2 },
 	{ "io_uring_setup", 425 },
@@ -217,6 +229,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "mmap2", __PNR_mmap2 },
 	{ "modify_ldt", __PNR_modify_ldt },
 	{ "mount", 40 },
+	{ "move_mount", 429 },
 	{ "move_pages", 239 },
 	{ "mprotect", 226 },
 	{ "mpx", __PNR_mpx },
@@ -224,7 +237,9 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "mq_notify", 184 },
 	{ "mq_open", 180 },
 	{ "mq_timedreceive", 183 },
+	{ "mq_timedreceive_time64", __PNR_mq_timedreceive_time64 },
 	{ "mq_timedsend", 182 },
+	{ "mq_timedsend_time64", __PNR_mq_timedsend_time64 },
 	{ "mq_unlink", 181 },
 	{ "mremap", 216 },
 	{ "msgctl", 187 },
@@ -249,6 +264,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "oldwait4", __PNR_oldwait4 },
 	{ "open", __PNR_open },
 	{ "open_by_handle_at", 265 },
+	{ "open_tree", 428 },
 	{ "openat", 56 },
 	{ "pause", __PNR_pause },
 	{ "pciconfig_iobase", __PNR_pciconfig_iobase },
@@ -256,6 +272,8 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "pciconfig_write", __PNR_pciconfig_write },
 	{ "perf_event_open", 241 },
 	{ "personality", 92 },
+	{ "pidfd_open", 434 },
+	{ "pidfd_send_signal", 424 },
 	{ "pipe", __PNR_pipe },
 	{ "pipe2", 59 },
 	{ "pivot_root", 41 },
@@ -264,6 +282,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "pkey_mprotect", 288 },
 	{ "poll", __PNR_poll },
 	{ "ppoll", 73 },
+	{ "ppoll_time64", __PNR_ppoll_time64 },
 	{ "prctl", 167 },
 	{ "pread64", 67 },
 	{ "preadv", 69 },
@@ -274,6 +293,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "prof", __PNR_prof },
 	{ "profil", __PNR_profil },
 	{ "pselect6", 72 },
+	{ "pselect6_time64", __PNR_pselect6_time64 },
 	{ "ptrace", 117 },
 	{ "putpmsg", __PNR_putpmsg },
 	{ "pwrite64", 68 },
@@ -291,6 +311,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "recv", __PNR_recv },
 	{ "recvfrom", 207 },
 	{ "recvmmsg", 243 },
+	{ "recvmmsg_time64", __PNR_recvmmsg_time64 },
 	{ "recvmsg", 212 },
 	{ "remap_file_pages", 234 },
 	{ "removexattr", 14 },
@@ -308,6 +329,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "rt_sigreturn", 139 },
 	{ "rt_sigsuspend", 133 },
 	{ "rt_sigtimedwait", 137 },
+	{ "rt_sigtimedwait_time64", __PNR_rt_sigtimedwait_time64 },
 	{ "rt_tgsigqueueinfo", 240 },
 	{ "rtas", __PNR_rtas },
 	{ "s390_guarded_storage", __PNR_s390_guarded_storage },
@@ -322,6 +344,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "sched_getparam", 121 },
 	{ "sched_getscheduler", 120 },
 	{ "sched_rr_get_interval", 127 },
+	{ "sched_rr_get_interval_time64", __PNR_sched_rr_get_interval_time64 },
 	{ "sched_setaffinity", 122 },
 	{ "sched_setattr", 274 },
 	{ "sched_setparam", 118 },
@@ -334,6 +357,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "semget", 190 },
 	{ "semop", 193 },
 	{ "semtimedop", 192 },
+	{ "semtimedop_time64", __PNR_semtimedop_time64 },
 	{ "send", __PNR_send },
 	{ "sendfile", 71 },
 	{ "sendfile64", __PNR_sendfile64 },
@@ -367,7 +391,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "setresuid32", __PNR_setresuid32 },
 	{ "setreuid", 145 },
 	{ "setreuid32", __PNR_setreuid32 },
-	{ "setrlimit", 164 },
+	{ "setrlimit", __PNR_setrlimit },
 	{ "setsid", 157 },
 	{ "setsockopt", 208 },
 	{ "settimeofday", 170 },
@@ -427,11 +451,15 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "timer_delete", 111 },
 	{ "timer_getoverrun", 109 },
 	{ "timer_gettime", 108 },
+	{ "timer_gettime64", __PNR_timer_gettime64 },
 	{ "timer_settime", 110 },
+	{ "timer_settime64", __PNR_timer_settime64 },
 	{ "timerfd", __PNR_timerfd },
 	{ "timerfd_create", 85 },
 	{ "timerfd_gettime", 87 },
+	{ "timerfd_gettime64", __PNR_timerfd_gettime64 },
 	{ "timerfd_settime", 86 },
+	{ "timerfd_settime64", __PNR_timerfd_settime64 },
 	{ "times", 153 },
 	{ "tkill", 130 },
 	{ "truncate", 45 },
@@ -453,6 +481,7 @@ const struct arch_syscall_def aarch64_syscall_table[] = { \
 	{ "ustat", __PNR_ustat },
 	{ "utime", __PNR_utime },
 	{ "utimensat", 88 },
+	{ "utimensat_time64", __PNR_utimensat_time64 },
 	{ "utimes", __PNR_utimes },
 	{ "vfork", __PNR_vfork },
 	{ "vhangup", 58 },

src/arch-arm-syscalls.c
@@ -37,7 +37,7 @@
 #define __SCMP_NR_BASE			__SCMP_NR_OABI_SYSCALL_BASE
 #endif
 
-/* NOTE: based on Linux 4.15-rc7 */
+/* NOTE: based on Linux v5.4-rc4 */
 const struct arch_syscall_def arm_syscall_table[] = { \
 	/* NOTE: arm_sync_file_range() and sync_file_range2() share values */
 	{ "_llseek", (__SCMP_NR_BASE + 140) },
@@ -70,11 +70,17 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "chown32", (__SCMP_NR_BASE + 212) },
 	{ "chroot", (__SCMP_NR_BASE + 61) },
 	{ "clock_adjtime", (__SCMP_NR_BASE + 372) },
+	{ "clock_adjtime64", (__SCMP_NR_BASE + 405) },
 	{ "clock_getres", (__SCMP_NR_BASE + 264) },
+	{ "clock_getres_time64", (__SCMP_NR_BASE + 406) },
 	{ "clock_gettime", (__SCMP_NR_BASE + 263) },
+	{ "clock_gettime64", (__SCMP_NR_BASE + 403) },
 	{ "clock_nanosleep", (__SCMP_NR_BASE + 265) },
+	{ "clock_nanosleep_time64", (__SCMP_NR_BASE + 407) },
 	{ "clock_settime", (__SCMP_NR_BASE + 262) },
+	{ "clock_settime64", (__SCMP_NR_BASE + 404) },
 	{ "clone", (__SCMP_NR_BASE + 120) },
+	{ "clone3", (__SCMP_NR_BASE + 435) },
 	{ "close", (__SCMP_NR_BASE +  6) },
 	{ "connect", (__SCMP_NR_BASE + 283) },
 	{ "copy_file_range", (__SCMP_NR_BASE + 391) },
@@ -118,7 +124,11 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "flock", (__SCMP_NR_BASE + 143) },
 	{ "fork", (__SCMP_NR_BASE +  2) },
 	{ "fremovexattr", (__SCMP_NR_BASE + 237) },
+	{ "fsconfig", (__SCMP_NR_BASE + 431) },
 	{ "fsetxattr", (__SCMP_NR_BASE + 228) },
+	{ "fsmount", (__SCMP_NR_BASE + 432) },
+	{ "fsopen", (__SCMP_NR_BASE + 430) },
+	{ "fspick", (__SCMP_NR_BASE + 433) },
 	{ "fstat", (__SCMP_NR_BASE + 108) },
 	{ "fstat64", (__SCMP_NR_BASE + 197) },
 	{ "fstatat64", (__SCMP_NR_BASE + 327) },
@@ -129,6 +139,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "ftruncate", (__SCMP_NR_BASE + 93) },
 	{ "ftruncate64", (__SCMP_NR_BASE + 194) },
 	{ "futex", (__SCMP_NR_BASE + 240) },
+	{ "futex_time64", (__SCMP_NR_BASE + 422) },
 	{ "futimesat", (__SCMP_NR_BASE + 326) },
 	{ "get_kernel_syms", __PNR_get_kernel_syms },
 	{ "get_mempolicy", (__SCMP_NR_BASE + 320) },
@@ -181,6 +192,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "io_destroy", (__SCMP_NR_BASE + 244) },
 	{ "io_getevents", (__SCMP_NR_BASE + 245) },
 	{ "io_pgetevents", (__SCMP_NR_BASE + 399) },
+	{ "io_pgetevents_time64", (__SCMP_NR_BASE + 416) },
 	{ "io_setup", (__SCMP_NR_BASE + 243) },
 	{ "io_submit", (__SCMP_NR_BASE + 246) },
 	{ "io_uring_setup", (__SCMP_NR_BASE + 425) },
@@ -193,7 +205,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "ioprio_set", (__SCMP_NR_BASE + 314) },
 	{ "ipc", __PNR_ipc },
 	{ "kcmp", (__SCMP_NR_BASE + 378) },
-	{ "kexec_file_load", __PNR_kexec_file_load },
+	{ "kexec_file_load", (__SCMP_NR_BASE + 401) },
 	{ "kexec_load", (__SCMP_NR_BASE + 347) },
 	{ "keyctl", (__SCMP_NR_BASE + 311) },
 	{ "kill", (__SCMP_NR_BASE + 37) },
@@ -216,7 +228,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "mbind", (__SCMP_NR_BASE + 319) },
 	{ "membarrier",  (__SCMP_NR_BASE + 389) },
 	{ "memfd_create", (__SCMP_NR_BASE + 385) },
-	{ "migrate_pages", __PNR_migrate_pages },
+	{ "migrate_pages", (__SCMP_NR_BASE + 400) },
 	{ "mincore", (__SCMP_NR_BASE + 219) },
 	{ "mkdir", (__SCMP_NR_BASE + 39) },
 	{ "mkdirat", (__SCMP_NR_BASE + 323) },
@@ -229,6 +241,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "mmap2", (__SCMP_NR_BASE + 192) },
 	{ "modify_ldt", __PNR_modify_ldt },
 	{ "mount", (__SCMP_NR_BASE + 21) },
+	{ "move_mount", (__SCMP_NR_BASE + 429) },
 	{ "move_pages", (__SCMP_NR_BASE + 344) },
 	{ "mprotect", (__SCMP_NR_BASE + 125) },
 	{ "mpx", __PNR_mpx },
@@ -236,7 +249,9 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "mq_notify", (__SCMP_NR_BASE + 278) },
 	{ "mq_open", (__SCMP_NR_BASE + 274) },
 	{ "mq_timedreceive", (__SCMP_NR_BASE + 277) },
+	{ "mq_timedreceive_time64", (__SCMP_NR_BASE + 419) },
 	{ "mq_timedsend", (__SCMP_NR_BASE + 276) },
+	{ "mq_timedsend_time64", (__SCMP_NR_BASE + 418) },
 	{ "mq_unlink", (__SCMP_NR_BASE + 275) },
 	{ "mremap", (__SCMP_NR_BASE + 163) },
 	{ "msgctl", (__SCMP_NR_BASE + 304) },
@@ -261,6 +276,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "oldwait4", __PNR_oldwait4 },
 	{ "open", (__SCMP_NR_BASE +  5) },
 	{ "open_by_handle_at", (__SCMP_NR_BASE + 371) },
+	{ "open_tree", (__SCMP_NR_BASE + 428) },
 	{ "openat", (__SCMP_NR_BASE + 322) },
 	{ "pause", (__SCMP_NR_BASE + 29) },
 	{ "pciconfig_iobase", (__SCMP_NR_BASE + 271) },
@@ -268,6 +284,8 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "pciconfig_write", (__SCMP_NR_BASE + 273) },
 	{ "perf_event_open", (__SCMP_NR_BASE + 364) },
 	{ "personality", (__SCMP_NR_BASE + 136) },
+	{ "pidfd_open", (__SCMP_NR_BASE + 434) },
+	{ "pidfd_send_signal", (__SCMP_NR_BASE + 424) },
 	{ "pipe", (__SCMP_NR_BASE + 42) },
 	{ "pipe2", (__SCMP_NR_BASE + 359) },
 	{ "pivot_root", (__SCMP_NR_BASE + 218) },
@@ -276,6 +294,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "pkey_mprotect", (__SCMP_NR_BASE + 394) },
 	{ "poll", (__SCMP_NR_BASE + 168) },
 	{ "ppoll", (__SCMP_NR_BASE + 336) },
+	{ "ppoll_time64", (__SCMP_NR_BASE + 414) },
 	{ "prctl", (__SCMP_NR_BASE + 172) },
 	{ "pread64", (__SCMP_NR_BASE + 180) },
 	{ "preadv", (__SCMP_NR_BASE + 361) },
@@ -286,6 +305,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "prof", __PNR_prof },
 	{ "profil", __PNR_profil },
 	{ "pselect6", (__SCMP_NR_BASE + 335) },
+	{ "pselect6_time64", (__SCMP_NR_BASE + 413) },
 	{ "ptrace", (__SCMP_NR_BASE + 26) },
 	{ "putpmsg", __PNR_putpmsg },
 	{ "pwrite64", (__SCMP_NR_BASE + 181) },
@@ -303,6 +323,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "recv", (__SCMP_NR_BASE + 291) },
 	{ "recvfrom", (__SCMP_NR_BASE + 292) },
 	{ "recvmmsg", (__SCMP_NR_BASE + 365) },
+	{ "recvmmsg_time64", (__SCMP_NR_BASE + 417) },
 	{ "recvmsg", (__SCMP_NR_BASE + 297) },
 	{ "remap_file_pages", (__SCMP_NR_BASE + 253) },
 	{ "removexattr", (__SCMP_NR_BASE + 235) },
@@ -320,6 +341,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "rt_sigreturn", (__SCMP_NR_BASE + 173) },
 	{ "rt_sigsuspend", (__SCMP_NR_BASE + 179) },
 	{ "rt_sigtimedwait", (__SCMP_NR_BASE + 177) },
+	{ "rt_sigtimedwait_time64", (__SCMP_NR_BASE + 421) },
 	{ "rt_tgsigqueueinfo", (__SCMP_NR_BASE + 363) },
 	{ "rtas", __PNR_rtas },
 	{ "s390_guarded_storage", __PNR_s390_guarded_storage },
@@ -334,6 +356,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "sched_getparam", (__SCMP_NR_BASE + 155) },
 	{ "sched_getscheduler", (__SCMP_NR_BASE + 157) },
 	{ "sched_rr_get_interval", (__SCMP_NR_BASE + 161) },
+	{ "sched_rr_get_interval_time64", (__SCMP_NR_BASE + 423) },
 	{ "sched_setaffinity", (__SCMP_NR_BASE + 241) },
 	{ "sched_setattr", (__SCMP_NR_BASE + 380) },
 	{ "sched_setparam", (__SCMP_NR_BASE + 154) },
@@ -346,6 +369,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "semget", (__SCMP_NR_BASE + 299) },
 	{ "semop", (__SCMP_NR_BASE + 298) },
 	{ "semtimedop", (__SCMP_NR_BASE + 312) },
+	{ "semtimedop_time64", (__SCMP_NR_BASE + 420) },
 	{ "send", (__SCMP_NR_BASE + 289) },
 	{ "sendfile", (__SCMP_NR_BASE + 187) },
 	{ "sendfile64", (__SCMP_NR_BASE + 239) },
@@ -439,11 +463,15 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "timer_delete", (__SCMP_NR_BASE + 261) },
 	{ "timer_getoverrun", (__SCMP_NR_BASE + 260) },
 	{ "timer_gettime", (__SCMP_NR_BASE + 259) },
+	{ "timer_gettime64", (__SCMP_NR_BASE + 408) },
 	{ "timer_settime", (__SCMP_NR_BASE + 258) },
+	{ "timer_settime64", (__SCMP_NR_BASE + 409) },
 	{ "timerfd", __PNR_timerfd },
 	{ "timerfd_create", (__SCMP_NR_BASE + 350) },
 	{ "timerfd_gettime", (__SCMP_NR_BASE + 354) },
+	{ "timerfd_gettime64", (__SCMP_NR_BASE + 410) },
 	{ "timerfd_settime", (__SCMP_NR_BASE + 353) },
+	{ "timerfd_settime64", (__SCMP_NR_BASE + 411) },
 	{ "times", (__SCMP_NR_BASE + 43) },
 	{ "tkill", (__SCMP_NR_BASE + 238) },
 	{ "truncate", (__SCMP_NR_BASE + 92) },
@@ -465,6 +493,7 @@ const struct arch_syscall_def arm_syscall_table[] = { \
 	{ "ustat", (__SCMP_NR_BASE + 62) },
 	{ "utime", __PNR_utime },
 	{ "utimensat", (__SCMP_NR_BASE + 348) },
+	{ "utimensat_time64", (__SCMP_NR_BASE + 412) },
 	{ "utimes", (__SCMP_NR_BASE + 269) },
 	{ "vfork", (__SCMP_NR_BASE + 190) },
 	{ "vhangup", (__SCMP_NR_BASE + 111) },

commit 2e54815cd843687d750cc9822f992389bb7b76cd

api: define __SNR_ppoll again
Commit bf747eb21e428c2b3ead6ebcca27951b681963a0 accidentally removed the
__SNR_ppoll definition. Add it back, using a PNR value if disabled in
the kernel headers.

Signed-off-by: Miroslav Lichvar <mlichvar@redhat.com>

include/seccomp-syscalls.h
@@ -272,6 +272,7 @@
 #define __PNR_timerfd_gettime64			-10238
 #define __PNR_timerfd_settime64			-10239
 #define __PNR_utimensat_time64			-10240
+#define __PNR_ppoll				-10241
 
 /*
  * libseccomp syscall definitions
@@ -1357,6 +1358,12 @@
 #define __SNR_poll			__NR_poll
 #else
 #define __SNR_poll			__PNR_poll
+#endif
+
+#ifdef __NR_ppoll
+#define __SNR_ppoll			__NR_ppoll
+#else
+#define __SNR_ppoll			__PNR_ppoll
 #endif
 
 #ifdef __NR_ppoll_time64