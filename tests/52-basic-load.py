138299c6bb22f0638dcbf2c2e915270ad66d008c
Paul Moore <paul@paul-moore.com>
2020-01-27 09:38:38 -0700

    tests: introduce a simple seccomp_load() test as part of the non-live tests
    This is a bit controversial as historically we've refrained from
    doing any tests that rely on the host kernel in the non-live tests,
    but I think enough time has past that we can do a simple
    seccomp_load() and not break the world's build/test platforms.
    
    The obvious big advantage is we are now testing the basic
    prctl()/seccomp() filter load infrastructure as part of the main
    regression test run.
    
    Acked-by: Tom Hromatka <tom.hromatka@oracle.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    (cherry picked from commit 3bae8850487eaad54fccbf206516b269f330b08f)
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
    Acked-by: Paul Moore <paul@paul-moore.com>
    
    Conflicts:
    	tests/.gitignore
    	tests/Makefile.am

---
+++ tests/52-basic-load.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#
+# Seccomp Library test program
+#
+# Copyright (c) 2019 Cisco Systems, Inc. <pmoore2@cisco.com>
+# Author: Paul Moore <paul@paul-moore.com>
+#
+
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of version 2.1 of the GNU Lesser General Public License as
+# published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, see <http://www.gnu.org/licenses>.
+#
+
+import argparse
+import sys
+
+import util
+
+from seccomp import *
+
+def test():
+    f = SyscallFilter(ALLOW)
+    f.load()
+
+test()
+
+# kate: syntax python;
+# kate: indent-mode python; space-indent on; indent-width 4; mixedindent off;
