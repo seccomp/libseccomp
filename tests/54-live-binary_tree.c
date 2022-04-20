4562a222ccf1d042899317c554c31a712f8e813c
Tom Hromatka <tom.hromatka@oracle.com>
2022-04-20 13:37:02 -0600

    tests: Fix make check-syntax error in test 54
    54-live-binary_tree.c had spaces rather than tabs on
    three lines.  Convert them to tabs.
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>
    (cherry picked from commit a379a4e5cb32d79e6098d25b51506c284c820c1a)

--- tests/54-live-binary_tree.c
+++ tests/54-live-binary_tree.c
@@ -86,9 +86,9 @@ int main(int argc, char *argv[])
 	if (ctx == NULL)
 		return ENOMEM;
 
-        rc = seccomp_attr_set(ctx, SCMP_FLTATR_CTL_OPTIMIZE, 2);
-        if (rc < 0)
-                goto out;
+	rc = seccomp_attr_set(ctx, SCMP_FLTATR_CTL_OPTIMIZE, 2);
+	if (rc < 0)
+		goto out;
 	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 1,
 			      SCMP_A0(SCMP_CMP_EQ, fd));
 	if (rc != 0)
