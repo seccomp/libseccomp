seccomp/libseccomp

commit e797591bdd6834272e2db292400f608ed9bd7fab

all: add seccomp_precompute() functionality
This patch adds a seccomp_precompute() API to precompute the seccomp
filter prior to calling seccomp_load() or similar functions.  Not
only does this improve the performance of seccomp_load(), it ensures
that seccomp_load() is async-signal-safe if no additional changes
have been made since the filter was precomputed.

Python bindings, test, and manpage updates are included in this
patch.

One minor side effect of this change is that seccomp_export_bpf_mem()
now always return the length of the filter in the "len" function
parameter, even in cases where the passed buffer is too small.
Arguably seccomp_export_bpf_mem() should have always behaved this
way.

Signed-off-by: Paul Moore <paul@paul-moore.com>

src/api.c
@@ -2,6 +2,7 @@
  * Seccomp Library API
  *
  * Copyright (c) 2012,2013 Red Hat <pmoore@redhat.com>
+ * Copyright (c) 2022 Microsoft Corporation <paulmoore@microsoft.com>
  * Author: Paul Moore <paul@paul-moore.com>
  */
 
@@ -723,11 +724,11 @@ API int seccomp_export_bpf(const scmp_filter_ctx ctx, int fd)
 		return _rc_filter(-EINVAL);
 	col = (struct db_filter_col *)ctx;
 
-	rc = gen_bpf_generate(col, &program);
+	rc = db_col_precompute(col);
 	if (rc < 0)
 		return _rc_filter(rc);
+	program = col->prgm_bpf;
 	rc = write(fd, program->blks, BPF_PGM_SIZE(program));
-	gen_bpf_release(program);
 	if (rc < 0)
 		return _rc_filter_sys(col, -errno);
 
@@ -739,7 +740,6 @@ API int seccomp_export_bpf_mem(const scmp_filter_ctx ctx, void *buf,
 			       size_t *len)
 {
 	int rc;
-	size_t buf_len;
 	struct db_filter_col *col;
 	struct bpf_program *program;
 
@@ -747,21 +747,31 @@ API int seccomp_export_bpf_mem(const scmp_filter_ctx ctx, void *buf,
 		return _rc_filter(-EINVAL);
 	col = (struct db_filter_col *)ctx;
 
-	rc = gen_bpf_generate(col, &program);
+	rc = db_col_precompute(col);
 	if (rc < 0)
 		return _rc_filter(rc);
-	buf_len = *len;
-	*len = BPF_PGM_SIZE(program);
+	program = col->prgm_bpf;
 
-	rc = 0;
 	if (buf) {
 		/* If we have a big enough buffer, write the program. */
-		if (*len > buf_len)
+		if (BPF_PGM_SIZE(program) > *len)
 			rc = _rc_filter(-ERANGE);
 		else
 			memcpy(buf, program->blks, *len);
 	}
-	gen_bpf_release(program);
+	*len = BPF_PGM_SIZE(program);
 
 	return rc;
+}
+
+/* NOTE - function header comment in include/seccomp.h */
+API int seccomp_precompute(const scmp_filter_ctx ctx)
+{
+	struct db_filter_col *col;
+
+	if (_ctx_valid(ctx))
+		return _rc_filter(-EINVAL);
+	col = (struct db_filter_col *)ctx;
+
+	return _rc_filter(db_col_precompute(col));
 }

doc/Makefile.am
@@ -33,6 +33,7 @@ dist_man3_MANS = \
 	man/man3/seccomp_init.3 \
 	man/man3/seccomp_load.3 \
 	man/man3/seccomp_merge.3 \
+	man/man3/seccomp_precompute.3 \
 	man/man3/seccomp_release.3 \
 	man/man3/seccomp_reset.3 \
 	man/man3/seccomp_rule_add.3 \

doc/man/man3/seccomp_precompute.3
@@ -1,8 +1,8 @@
-.TH "seccomp_load" 3 "30 May 2020" "paul@paul-moore.com" "libseccomp Documentation"
+.TH "seccomp_precompute" 3 "19 September 2022" "paul@paul-moore.com" "libseccomp Documentation"
 .\" //////////////////////////////////////////////////////////////////////////
 .SH NAME
 .\" //////////////////////////////////////////////////////////////////////////
-seccomp_load \- Load the current seccomp filter into the kernel
+seccomp_precompute \- Precompute the current seccomp filter
 .\" //////////////////////////////////////////////////////////////////////////
 .SH SYNOPSIS
 .\" //////////////////////////////////////////////////////////////////////////
@@ -11,7 +11,7 @@ seccomp_load \- Load the current seccomp filter into the kernel
 .sp
 .B typedef void * scmp_filter_ctx;
 .sp
-.BI "int seccomp_load(scmp_filter_ctx " ctx ");"
+.BI "int seccomp_precompute(scmp_filter_ctx " ctx ");"
 .sp
 Link with \fI\-lseccomp\fP.
 .fi
@@ -19,24 +19,12 @@ Link with \fI\-lseccomp\fP.
 .SH DESCRIPTION
 .\" //////////////////////////////////////////////////////////////////////////
 .P
-Loads the seccomp filter provided by
-.I ctx
-into the kernel; if the function
-succeeds the new seccomp filter will be active when the function returns.  If
-.BR seccomp_precompute (3)
-was called prior to attempting to load the seccomp filter, and no changes have
-been made to the filter,
+Precomputes the seccomp filter for later use by
 .BR seccomp_load ()
-can be considered to be async-signal-safe.
-.P
-As it is possible to have multiple stacked seccomp filters for a given task
-(defined as either a process or a thread), it is important to remember that
-each of the filters loaded for a given task are executed when a syscall is
-made and the "strictest" rule is the rule that is applied.  In the case of
-seccomp, "strictest" is defined as the action with the lowest value (e.g.
-.I SCMP_ACT_KILL
-is "stricter" than
-.IR SCMP_ACT_ALLOW ).
+and similar functions.  Not only does this improve performance of
+.BR seccomp_load ()
+it also ensures that the seccomp filter can be loaded in an async-signal-safe
+manner if no changes have been made to the filter since it was precomputed.
 .\" //////////////////////////////////////////////////////////////////////////
 .SH RETURN VALUE
 .\" //////////////////////////////////////////////////////////////////////////
@@ -53,9 +41,6 @@ Invalid input, either the context or architecture token is invalid.
 .TP
 .B -ENOMEM
 The library was unable to allocate enough memory.
-.TP
-.B -ESRCH
-Unable to load the filter due to thread issues.
 .P
 If the \fISCMP_FLTATR_API_SYSRAWRC\fP filter attribute is non-zero then
 additional error codes may be returned to the caller; these additional error
@@ -74,6 +59,12 @@ int main(int argc, char *argv[])
 
 	ctx = seccomp_init(SCMP_ACT_KILL);
 	if (ctx == NULL)
+		goto out;
+
+	/* ... */
+
+	rc = seccomp_precompute(ctx);
+	if (rc < 0)
 		goto out;
 
 	/* ... */
@@ -108,10 +99,5 @@ Paul Moore <paul@paul-moore.com>
 .\" //////////////////////////////////////////////////////////////////////////
 .SH SEE ALSO
 .\" //////////////////////////////////////////////////////////////////////////
-.BR seccomp_init (3),
-.BR seccomp_reset (3),
-.BR seccomp_release (3),
-.BR seccomp_rule_add (3),
-.BR seccomp_rule_add_exact (3)
-.BR seccomp_precompute (3)
+.BR seccomp_load (3)
 .BR signal-safety (7)

include/seccomp.h.in
@@ -830,6 +830,17 @@ int seccomp_export_bpf(const scmp_filter_ctx ctx, int fd);
  */
 int seccomp_export_bpf_mem(const scmp_filter_ctx ctx, void *buf, size_t *len);
 
+/**
+ * Precompute the seccomp filter for future use
+ * @param ctx the filter context
+ *
+ * This function precomputes the seccomp filter and stores it internally for
+ * future use, speeding up seccomp_load() and other functions which require
+ * the generated filter.
+ *
+ */
+int seccomp_precompute(const scmp_filter_ctx ctx);
+
 /*
  * pseudo syscall definitions
  */

src/db.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2012,2016,2018 Red Hat <pmoore@redhat.com>
  * Copyright (c) 2019 Cisco Systems, Inc. <pmoore2@cisco.com>
+ * Copyright (c) 2022 Microsoft Corporation <paulmoore@microsoft.com>
  * Author: Paul Moore <paul@paul-moore.com>
  */
 
@@ -1098,6 +1099,9 @@ int db_col_reset(struct db_filter_col *col, uint32_t def_action)
 		free(snap);
 	}
 
+	/* reset the precomputed programs */
+	db_col_precompute_reset(col);
+
 	return 0;
 }
 
@@ -1161,6 +1165,9 @@ void db_col_release(struct db_filter_col *col)
 	if (col->filters)
 		free(col->filters);
 	col->filters = NULL;
+
+	/* free any precompute */
+	db_col_precompute_reset(col);
 
 	/* free the collection */
 	free(col);
@@ -1249,6 +1256,9 @@ int db_col_merge(struct db_filter_col *col_dst, struct db_filter_col *col_src)
 		col_dst->filters[iter_a] = col_src->filters[iter_b];
 		col_dst->filter_cnt++;
 	}
+
+	/* reset the precompute */
+	db_col_precompute_reset(col_dst);
 
 	/* free the source */
 	col_src->filter_cnt = 0;
@@ -1373,6 +1383,7 @@ int db_col_attr_set(struct db_filter_col *col,
 			col->attr.act_badarch = value;
 		else
 			return -EINVAL;
+		db_col_precompute_reset(col);
 		break;
 	case SCMP_FLTATR_CTL_NNP:
 		col->attr.nnp_enable = (value ? 1 : 0);
@@ -1394,6 +1405,7 @@ int db_col_attr_set(struct db_filter_col *col,
 		break;
 	case SCMP_FLTATR_API_TSKIP:
 		col->attr.api_tskip = (value ? 1 : 0);
+		db_col_precompute_reset(col);
 		break;
 	case SCMP_FLTATR_CTL_LOG:
 		rc = sys_chk_seccomp_flag(SECCOMP_FILTER_FLAG_LOG);
@@ -1427,6 +1439,7 @@ int db_col_attr_set(struct db_filter_col *col,
 			rc = -EOPNOTSUPP;
 			break;
 		}
+		db_col_precompute_reset(col);
 		break;
 	case SCMP_FLTATR_API_SYSRAWRC:
 		col->attr.api_sysrawrc = (value ? 1 : 0);
@@ -1460,6 +1473,8 @@ int db_col_db_new(struct db_filter_col *col, const struct arch_def *arch)
 	rc = db_col_db_add(col, db);
 	if (rc < 0)
 		_db_release(db);
+	else
+		db_col_precompute_reset(col);
 
 	return rc;
 }
@@ -1539,6 +1554,8 @@ int db_col_db_remove(struct db_filter_col *col, uint32_t arch_token)
 		col->filters = NULL;
 		col->endian = 0;
 	}
+
+	db_col_precompute_reset(col);
 
 	return 0;
 }
@@ -2233,6 +2250,9 @@ priority_failure:
 			rc = rc_tmp;
 	}
 
+	if (rc == 0)
+		db_col_precompute_reset(col);
+
 	return rc;
 }
 
@@ -2377,8 +2397,11 @@ add_arch_fail:
 
 add_return:
 	/* update the misc state */
-	if (rc == 0 && action == SCMP_ACT_NOTIFY)
-		col->notify_used = true;
+	if (rc == 0) {
+		if (action == SCMP_ACT_NOTIFY)
+			col->notify_used = true;
+		db_col_precompute_reset(col);
+	}
 	if (chain != NULL)
 		free(chain);
 	return rc;
@@ -2501,6 +2524,9 @@ void db_col_transaction_abort(struct db_filter_col *col)
 	for (iter = 0; iter < filter_cnt; iter++)
 		_db_release(filters[iter]);
 	free(filters);
+
+	/* free any precompute */
+	db_col_precompute_reset(col);
 }
 
 /**
@@ -2617,4 +2643,34 @@ shadow_err:
 	col->snapshots = snap->next;
 	_db_snap_release(snap);
 	return;
+}
+
+/**
+ * Precompute the seccomp filters
+ * @param col the filter collection
+ *
+ * This function precomputes the seccomp filters before they are needed,
+ * returns zero on success, negative values on error.
+ *
+ */
+int db_col_precompute(struct db_filter_col *col)
+{
+	if (!col->prgm_bpf)
+		return gen_bpf_generate(col, &col->prgm_bpf);
+	return 0;
+}
+
+/**
+ * Free any precomputed filter programs
+ * @param col the filter collection
+ *
+ * This function releases any precomputed filter programs.
+ */
+void db_col_precompute_reset(struct db_filter_col *col)
+{
+	if (!col->prgm_bpf)
+		return;
+
+	gen_bpf_release(col->prgm_bpf);
+	col->prgm_bpf = NULL;
 }

src/db.h
@@ -2,6 +2,7 @@
  * Enhanced Seccomp Filter DB
  *
  * Copyright (c) 2012,2016 Red Hat <pmoore@redhat.com>
+ * Copyright (c) 2022 Microsoft Corporation <paulmoore@microsoft.com>
  * Author: Paul Moore <paul@paul-moore.com>
  */
 
@@ -28,6 +29,7 @@
 #include <seccomp.h>
 
 #include "arch.h"
+#include "gen_bpf.h"
 
 /* XXX - need to provide doxygen comments for the types here */
 
@@ -162,6 +164,9 @@ struct db_filter_col {
 
 	/* userspace notification */
 	bool notify_used;
+
+	/* precomputed programs */
+	struct bpf_program *prgm_bpf;
 };
 
 /**
@@ -211,6 +216,9 @@ int db_col_syscall_priority(struct db_filter_col *col,
 int db_col_transaction_start(struct db_filter_col *col);
 void db_col_transaction_abort(struct db_filter_col *col);
 void db_col_transaction_commit(struct db_filter_col *col);
+
+int db_col_precompute(struct db_filter_col *col);
+void db_col_precompute_reset(struct db_filter_col *col);
 
 int db_rule_add(struct db_filter *db, const struct db_api_rule_list *rule);
 

src/python/libseccomp.pxd
@@ -170,5 +170,7 @@ cdef extern from "seccomp.h":
     int seccomp_export_bpf_mem(const scmp_filter_ctx ctx, void *buf,
                                size_t *len)
 
+    int seccomp_precompute(const scmp_filter_ctx ctx)
+
 # kate: syntax python;
 # kate: indent-mode python; space-indent on; indent-width 4; mixedindent off;

src/python/seccomp.pyx
@@ -1069,5 +1069,17 @@ cdef class SyscallFilter:
             raise RuntimeError(str.format("Library error (errno = {0})", rc))
         return program
 
+    def precompute(self):
+        """ Precompute the seccomp filter.
+
+        Description:
+        Precompute the seccomp filter and store it internally for future use,
+        speeding up filter loads and other functions which require the
+        generated filter.
+        """
+        rc = libseccomp.seccomp_precompute(self._ctx)
+        if rc != 0:
+            raise RuntimeError(str.format("Library error (errno = {0})", rc))
+
 # kate: syntax python;
 # kate: indent-mode python; space-indent on; indent-width 4; mixedindent off;

commit 904e9dee373eca499e976dce131f0baee06db2d6

api: fix seccomp_export_bpf_mem out-of-bounds read
*len is the length of the destination buffer, but program->blks is
probably not anywhere near that long.  It's already been checked above
that BPF_PGM_SIZE(program) is less than or equal to *len, so that's
the correct value to use here to avoid either reading or writing too
much.

I noticed this because tests/11-basic-basic_errors started failing on
musl after e797591 ("all: add seccomp_precompute() functionality").

Signed-off-by: Alyssa Ross <hi@alyssa.is>

src/api.c
@@ -786,7 +786,7 @@ API int seccomp_export_bpf_mem(const scmp_filter_ctx ctx, void *buf,
 		if (BPF_PGM_SIZE(program) > *len)
 			rc = _rc_filter(-ERANGE);
 		else
-			memcpy(buf, program->blks, *len);
+			memcpy(buf, program->blks, BPF_PGM_SIZE(program));
 	}
 	*len = BPF_PGM_SIZE(program);
 