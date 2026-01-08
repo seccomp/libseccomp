seccomp/libseccomp

commit 1a8730c5fb32e08c38efe49afe17375737ae687a

db: propagate notify_used when merging filter collections
Merging filter collections didn’t carry over notify_used; if the
source had SCMP_ACT_NOTIFY rules, the merged filter could skip requesting
SECCOMP_FILTER_FLAG_NEW_LISTENER, yielding no listener FD. This ORs the flag
from source to destination to keep seccomp notification working.

Signed-off-by: Joshua Rogers <libseccomp@joshua.hu>
Acked-by: Paul Moore <paul@paul-moore.com>
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/db.c
@@ -1259,6 +1259,10 @@ int db_col_merge(struct db_filter_col *col_dst, struct db_filter_col *col_src)
 	/* reset the precompute */
 	db_col_precompute_reset(col_dst);
 
+    /* propagate NOTIFY usage so NEW_LISTENER is requested on load */
+    if (col_src->notify_used)
+        col_dst->notify_used = true;
+
 	/* free the source */
 	col_src->filter_cnt = 0;
 	db_col_release(col_src);

commit 4b0bf5064cae3b4eac88020b31ce0ef43da0e3f9

db: Fix tab/space issue with previous commit
Replace spaces with tabs in previous commit.  There are no functional
changes in this commit.

Fixes: 1a8730c5fb32 ("db: propagate notify_used when merging filter collections")
Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

src/db.c
@@ -1259,9 +1259,9 @@ int db_col_merge(struct db_filter_col *col_dst, struct db_filter_col *col_src)
 	/* reset the precompute */
 	db_col_precompute_reset(col_dst);
 
-    /* propagate NOTIFY usage so NEW_LISTENER is requested on load */
-    if (col_src->notify_used)
-        col_dst->notify_used = true;
+	/* propagate NOTIFY usage so NEW_LISTENER is requested on load */
+	if (col_src->notify_used)
+		col_dst->notify_used = true;
 
 	/* free the source */
 	col_src->filter_cnt = 0;